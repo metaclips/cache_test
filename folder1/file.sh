85aba8ed3b1b0bfe4533e478ba43cf4223e9d15e
Michael Uti <utimichael9@gmail.com>
2022-01-18 00:04:18 +0100

    recreate test

---
+++ folder1/file.sh
@@ -0,0 +1,11 @@
+FILE="./file_mod.sh"
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else
+    touch file_mod.sh
+fi
+
+for i in {1..10000}; do
+    echo "hello"
+    echo "jjjjjjjjjjjjjjjjjj random string $i" >> file_mod.sh
+done
